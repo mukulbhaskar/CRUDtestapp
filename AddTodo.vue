6c7687fc85eeeec7f62b352e0e9b7558136fd15b
mukulbhaskar <83503556+mukulbhaskar@users.noreply.github.com>
2021-05-26 16:15:50 +0530

    Add files via upload

---
+++ AddTodo.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <br>
+        <h1 class="text-center">Games To Play</h1>
+        <br><br>
+        <form @submit="addTodo">
+        <div class="form-group">
+            <input required type="text" v-model="title" placeholder="title" name="title" class="form-control">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-block btn-danger">
+                Add Game to Play
+            </button>
+        </div>
+        </form>
+    </div>
+</template>
+<script>
+
+export default {
+  name: "AddTodo",
+  data(){
+      return{
+          title:""
+      }
+  },
+  methods:{
+      addTodo(e){
+          e.preventDefault()
+
+          const newTodo = {
+              id:Date.now(),
+              title:this.title,
+              completed:false
+          }
+
+          this.$emit("add-todo",newTodo)
+
+          this.title=""
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-on:delete-todo="deleteTodo" v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      todos:[
+        {
+          id:1,
+          title:"God of War",
+          completed:true
+        },
+        {
+          id:2,
+          title:"Monkey Island",
+          completed:false
+        },
+        {
+          id:3,
+          title:"Tetris",
+          completed:false
+        },
+        {
+          id:4,
+          title:"Valorant",
+          completed:false
+        },
+        {
+          id:5,
+          title:"DOOM",
+          completed:true
+        }
+      ]
+    }
+  },
+  components: {
+    Todos,
+    AddTodo
+  },
+  methods:{
+    addTodo(todo){
+      this.todos=[...this.todos,todo]
+    },
+    deleteTodo(id){
+      this.todos = this.todos.filter((todo) => todo.id !== id)
+
+    }
+  }
+}
+</script>
